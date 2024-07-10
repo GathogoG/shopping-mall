81589a6a0e7b0581eb175dfad32ba20714a76189
levis <levismunene01@gmail.com>
2024-07-10 15:54:20 +0300

    able to create tables

---
+++ backend/app.py
@@ -0,0 +1,33 @@
+from flask_migrate import Migrate
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+from models import db, User
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shopping.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+migrate = Migrate(app, db, render_as_batch=True)
+
+db.init_app(app)
+
+
+@app.route('/users', methods=['GET'])
+def get_users():
+
+
+    users = User.query.all()
+
+    result = []
+# Example route
+@app.route('/')
+def index():
+    return 'Welcome to the shopping site!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
