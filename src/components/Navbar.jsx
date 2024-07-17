caea1d4c429e7f56372888cedd0d198d976fdbf8
levis <levismunene01@gmail.com>
2024-07-17 12:02:05 +0300

    good proceding

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import cart_icon from '../Assets/cart_icon.png';
+import logo from '../Assets/logo.png';
+
+const Navbar = () => {
+  const [menu, setMenu] = useState("home");
+
+  return (
+    <div className='navbar'>
+      <div className="nav-logo">
+        <img src={logo} alt="logo" />
+        <p>Shopping Mall</p>
+      </div>
+
+      <ul className="nav-menu">
+        <li onClick={() => setMenu("home")}>
+          <Link to="/">Home</Link>
+          {menu === "home" ? <hr /> : <></>}
+        </li>
+        <li onClick={() => setMenu("shop")}>
+          Shop
+          {menu === "shop" ? <hr /> : <></>}
+        </li>
+        <li onClick={() => setMenu("about")}>
+          <Link to="/about"> About</Link>
+          {menu === "about" ? <hr /> : <></>}
+        </li>
+        <li onClick={() => setMenu("contact")}>
+          <Link to="/contact">Contact</Link>
+          {menu === "contact" ? <hr /> : <></>}
+        </li>
+      </ul>
+
+      <div className="nav-login-cart">
+        <Link to="/login">
+        <button>Login</button>
+        </Link>
+        <Link to="/register">
+        <button>Register</button>
+        </Link>
+        <img src={cart_icon} alt="cart icon" />
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
